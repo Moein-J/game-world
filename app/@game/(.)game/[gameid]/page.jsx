3854188ed2c16330b3a3aac27857ed745df6958e
maqz15 <moein.mj69@gmail.com>
2024-06-21 23:05:48 +0330

    refactoring game modal

--- app/@game/(.)game/[gameid]/page.jsx
+++ app/@game/(.)game/[gameid]/page.jsx
@@ -13,39 +13,41 @@ const page = ({ params }) => {
   const router = useRouter();
   return (
     <div className="fixed bg-[#121212] bg-opacity-95 flex flex-col w-screen backdrop-blur-sm z-50 h-screen justify-center items-center ">
-      <BackgroundGradient>
-        <div className="h-[60rem] w-[100rem] rounded-xl bg-[#121212] flex flex-col p-4 text-[#fff] gap-10">
-          <button
-            onClick={() => router.back()}
-            className="flex flex-col items-end"
-          >
-            <X
-              size={30}
-              className="hover:text-[#8f3d3d] text-[#fff] text-right"
-            />
-          </button>
-          <div className="overflow-y-auto flex flex-col overflow-x-hidden gap-8">
-            <div className="w-full flex p-4" id="game_info">
-              <div className="w-1/2 h-[30rem] relative ">
-                <Image
-                  src={"/RedDead.jpg"}
-                  fill
-                  alt="witcher3_picture"
-                  className="rounded-lg"
-                />
+      <div className="2xl:w-4/6 lg:w-4/5 xs:w-[90%]">
+        <BackgroundGradient>
+          <div className="h-[60rem] w-full rounded-xl bg-[#121212] flex flex-col p-4 text-[#fff] gap-10">
+            <button
+              onClick={() => router.back()}
+              className="flex flex-col items-end"
+            >
+              <X
+                size={30}
+                className="hover:text-[#8f3d3d] text-[#fff] text-right"
+              />
+            </button>
+            <div className="overflow-y-auto flex flex-col overflow-x-hidden gap-8">
+              <div className="w-full flex p-4" id="game_info">
+                <div className="w-1/2 h-[30rem] relative ">
+                  <Image
+                    src={"/RedDead.jpg"}
+                    fill
+                    alt="witcher3_picture"
+                    className="rounded-lg"
+                  />
+                </div>
+                {/* TITLE AND DETALS SECTION */}
+                <Title game={game} />
+                {/* ******** */}
               </div>
-              {/* TITLE AND DETALS SECTION */}
-              <Title game={game} />
-              {/* ******** */}
+              {/* ACCORDION SECTION */}
+              <AccordionContainer game={game} />
+              {/* SYSTEM REQUREMENTS SECTION */}
+              <Requirements requirements={game.requirements} />
+              <Download repacks={game.repacks} />
             </div>
-            {/* ACCORDION SECTION */}
-            <AccordionContainer game={game} />
-            {/* SYSTEM REQUREMENTS SECTION */}
-            <Requirements requirements={game.requirements} />
-            <Download repacks={game.repacks}/>
           </div>
-        </div>
-      </BackgroundGradient>
+        </BackgroundGradient>
+      </div>
     </div>
   );
 };
