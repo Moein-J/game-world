e42e3bd3328af1d58eb9af84d1ddfdfd29b7039a
maqz15 <moein.mj69@gmail.com>
2024-08-03 13:45:47 +0330

    build profile page

--- app/profile/layout.jsx
+++ app/profile/layout.jsx
@@ -1,13 +1,22 @@
-import Panel from "@/components/profile/panel";
+import Panel from "@/components/profile/Profile-Panel";
+import { userProvider } from "@/lib/actions";
+import Link from "next/link";
 
-const layout = ({ children }) => {
+const layout = async ({ children }) => {
+  const user = await userProvider();
   return (
     <>
-      <div className="h-screen bg-[#121212] p-11">
-        <div className="flex justify-end w-full h-full rounded-2xl bg-[#242529]">
-          <div className="p-10 w-full h-full text-right">{children}</div>
-          <Panel />
+      <div className="h-screen bg-[#121212] flex">
+        <div className="flex flex-col pb-8 pl-20 w-11/12 h-full">
+          <div className="w-full flex h-24">
+      
+          </div>
+          <div className="flex justify-end flex-grow rounded-2xl bg-[#242529]">
+            <div className="w-full h-full text-right">{children}</div>
+          </div>
         </div>
+
+        <Panel user={user} />
       </div>
     </>
   );
