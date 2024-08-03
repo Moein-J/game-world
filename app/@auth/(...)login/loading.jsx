61104cc421e60f4accc7e55c6fbf007611a79f05
maqz15 <moein.mj69@gmail.com>
2024-08-04 00:50:07 +0330

    authorized setup

--- app/@auth/(...)login/loading.jsx
+++ app/@auth/(...)login/loading.jsx
@@ -1,7 +1,9 @@
 const Loading = () => {
   return (
     <>
-      <div className="w-screen h-screen bg-textOrange backdrop-blur-sm bg-opacity-95">HIIIIIIIIII</div>
+      <div className="flex justify-center rounded-md items-center w-40 h-16 absolute left-7 bottom-7 text-links bg-bgSecondary backdrop-blur-sm bg-opacity-95 z-50">
+        ... درحال بازگزاری
+      </div>
     </>
   );
 };
